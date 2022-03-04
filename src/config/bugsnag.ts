633cee99aaefd1a524f8b387bc5ea41dda90a32c
Tim Morgan <git@timothymorgan.info>
2022-03-03 17:12:52 -0800

    Yarn 3, package updates, bump Node

--- src/config/bugsnag.ts
+++ src/config/bugsnag.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import Bugsnag from '@bugsnag/js'
+import Bugsnag, { Plugin } from '@bugsnag/js'
 import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
 Bugsnag.start({
   apiKey: '29094f9e852387f9cf1e6525b93d0ce9',
-  plugins: [new BugsnagPluginVue()],
+  plugins: [<Plugin> new BugsnagPluginVue()],
   enabledReleaseStages: ['production']
 })
 
